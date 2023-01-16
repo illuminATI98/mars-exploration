7027120cb97ffd7fdf89c114149037e93579f14b
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-16 23:16:41 +0100

    add AStarAlgorithm.cs interface

---
+++ Codecool.MarsExploration.MapExplorer/Pathfinder/AStarAlgorithm.cs
@@ -0,0 +1,10 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Pathfinder;
+
+public interface AStarAlgorithm
+{
+    public int GCost(Coordinate startingNode, Coordinate targetNode);
+    public int HCost(Coordinate endNode, Coordinate targetNode);
+    public int FCost(int gCost, int hCost);
+}
