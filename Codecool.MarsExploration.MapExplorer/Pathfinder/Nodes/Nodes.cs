d7174af1783d189cb4f4e4ff60020f617fc1452b
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-17 22:40:25 +0100

    add node array object

---
+++ Codecool.MarsExploration.MapExplorer/Pathfinder/Nodes/Nodes.cs
@@ -0,0 +1,26 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Pathfinder;
+
+public class Nodes
+{
+    private Node[,] _nodes;
+    
+    public void CreateNodeArray(string[,] map, int mapSize)
+    {
+        _nodes = new Node[mapSize, mapSize];
+
+        for (var i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                _nodes[i, j] = CreateNode(i, j, map[i, j]);
+            }
+        }
+    }
+    
+    private Node CreateNode(int xCoord, int yCoord, string representation)
+    {
+        return new Node(representation == " ", new Coordinate(xCoord, yCoord));
+    }
+}
