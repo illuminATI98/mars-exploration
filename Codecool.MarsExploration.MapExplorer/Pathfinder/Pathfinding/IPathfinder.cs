7fabad3896be23b6997105430d344eba036157e3
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-17 22:51:26 +0100

    add pathfinder interface

---
+++ Codecool.MarsExploration.MapExplorer/Pathfinder/Pathfinding/IPathfinder.cs
@@ -0,0 +1,8 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Pathfinder.Pathfinding;
+
+public interface IPathfinder
+{
+    IEnumerable<Coordinate> FindPath(Coordinate startingPos, Coordinate targetPos);
+}
