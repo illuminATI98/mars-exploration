1e562a0c595a36879cec8559262ab8d17ad7a5b7
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-17 22:40:09 +0100

    add node object

---
+++ Codecool.MarsExploration.MapExplorer/Pathfinder/Nodes/Node.cs
@@ -0,0 +1,16 @@
+﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Pathfinder;
+
+public class Node
+{
+    private bool _traversable;
+    private Coordinate _mapPosition;
+
+
+    public Node(bool traversable, Coordinate mapPosition)
+    {
+        _traversable = traversable;
+        _mapPosition = mapPosition;
+    }
+}
