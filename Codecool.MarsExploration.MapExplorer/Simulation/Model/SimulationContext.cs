c3f3da38fd5ed8707e38c34eadb07eb31795091d
Attila Dobi <attiladobi100@gmail.com>
2023-01-18 17:27:35 +0100

    Extend Context with constructions

--- Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
+++ Codecool.MarsExploration.MapExplorer/Simulation/Model/SimulationContext.cs
@@ -2,8 +2,8 @@ using Codecool.MarsExploration.MapExplorer.Exploration;
 using Codecool.MarsExploration.MapExplorer.MarsRover;
 using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 using Codecool.MarsExploration.MapGenerator.MapElements.Model;
-
+using Codecool.MarsExploration.MapExplorer.Construction;
 
 namespace Codecool.MarsExploration.MapExplorer.Simulation.Model;
 
-public record SimulationContext(int Step, int StepsToReachTimeOut, IEnumerable<Rover> Rovers,IEnumerable<CommandCenter.Model.CommandCenter> CommandCenters, Coordinate LocationOfTheSpaceship, Map Map, IEnumerable<string> SymbolsToLookFor, ExplorationOutcome ExplorationOutcome = ExplorationOutcome.InProgress);
+public record SimulationContext(int Step, int StepsToReachTimeOut, IEnumerable<Rover> Rovers,IEnumerable<CommandCenter.Model.CommandCenter> CommandCenters,IEnumerable<Construction.Model.Construction> Constructions,Coordinate LocationOfTheSpaceship, Map Map, IEnumerable<string> SymbolsToLookFor, ExplorationOutcome ExplorationOutcome = ExplorationOutcome.InProgress);
