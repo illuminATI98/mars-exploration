005c037fb272afe673a08ae4d8b6734737d2f436
Attila Dobi <attiladobi100@gmail.com>
2023-01-18 17:26:33 +0100

    Extend ILogger interface and ConsoleLogger class with new methods

--- Codecool.MarsExploration.MapExplorer/Logger/ILogger.cs
+++ Codecool.MarsExploration.MapExplorer/Logger/ILogger.cs
@@ -1,6 +1,14 @@
-﻿namespace Codecool.MarsExploration.MapExplorer.Logger;
+﻿using Codecool.MarsExploration.MapExplorer.MarsRover;
+using Codecool.MarsExploration.MapExplorer.Simulation.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Logger;
 
 public interface ILogger
 {
     void Log(string message);
+    void Position(Rover rover);
+    void Position(CommandCenter.Model.CommandCenter commandCenter);
+    void Step(SimulationContext simulationContext);
+    void OutCome(SimulationContext simulationContext);
+    void Construction(Construction.Model.Construction simulationContext);
 }
