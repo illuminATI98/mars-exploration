c52bb580dafa1ffda9aa63cbdb1f7d192c7929ab
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-19 12:07:12 +0100

    add node object type to calculate distance with

--- Codecool.MarsExploration.MapExplorer/Pathfinder/Nodes/Node.cs
+++ Codecool.MarsExploration.MapExplorer/Pathfinder/Nodes/Node.cs
@@ -4,13 +4,16 @@ namespace Codecool.MarsExploration.MapExplorer.Pathfinder;
 
 public class Node
 {
-    private bool _traversable;
-    private Coordinate _mapPosition;
-
+    public bool Traversable;
+    public readonly Coordinate MapPosition;
+    public Node Parent;
+    public int GCost { get; set;}
+    public int HCost { get; set;}
+    public int FCost => GCost + HCost;
 
     public Node(bool traversable, Coordinate mapPosition)
     {
-        _traversable = traversable;
-        _mapPosition = mapPosition;
+        Traversable = traversable;
+        MapPosition = mapPosition;
     }
 }
