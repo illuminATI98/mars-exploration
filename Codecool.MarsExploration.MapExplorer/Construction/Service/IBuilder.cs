811039be56235a113bb91407304fadae9003d17e
Attila Dobi <attiladobi100@gmail.com>
2023-01-19 18:09:44 +0100

    Create IBuilder interface and Builder class

---
+++ Codecool.MarsExploration.MapExplorer/Construction/Service/IBuilder.cs
@@ -0,0 +1,9 @@
+using System.Collections.Immutable;
+using Codecool.MarsExploration.MapExplorer.Simulation.Model;
+
+namespace Codecool.MarsExploration.MapExplorer.Construction.Service;
+
+public interface IBuilder
+{
+    SimulationContext Build(SimulationContext simulationContext,Model.Construction construction);
+}
