6432dc151b6c37bb15be94b8e5422672fdf211f4
Dorian Szatmari <dorian.szatmari@gmail.com>
2023-01-19 16:07:43 +0100

    implement heap interface in node object

--- Codecool.MarsExploration.MapExplorer/Pathfinder/Nodes/Node.cs
+++ Codecool.MarsExploration.MapExplorer/Pathfinder/Nodes/Node.cs
@@ -1,8 +1,9 @@
-﻿using Codecool.MarsExploration.MapGenerator.Calculators.Model;
+﻿using Codecool.MarsExploration.MapExplorer.Pathfinder.Optimisation;
+using Codecool.MarsExploration.MapGenerator.Calculators.Model;
 
 namespace Codecool.MarsExploration.MapExplorer.Pathfinder;
 
-public class Node
+public class Node : IHeap<Node>
 {
     public bool Traversable;
     public readonly Coordinate MapPosition;
@@ -10,10 +11,27 @@ public class Node
     public int GCost { get; set;}
     public int HCost { get; set;}
     public int FCost => GCost + HCost;
-
+    private int _heapIndex;
     public Node(bool traversable, Coordinate mapPosition)
     {
         Traversable = traversable;
         MapPosition = mapPosition;
+    }
+
+    public int CompareTo(Node? nodeToCompare)
+    {
+        int compare = FCost.CompareTo(nodeToCompare.FCost);
+        if (compare == 0)
+        {
+            compare = HCost.CompareTo(nodeToCompare.HCost);
+        }
+
+        return -compare;
+    }
+
+    public int HeapIndex
+    {
+        get { return _heapIndex;}
+        set { _heapIndex = value; }
     }
 }
