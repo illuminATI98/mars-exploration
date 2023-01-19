b634aa41f481a64bce0211a26744ad78778cf0c0
Attila Dobi <attiladobi100@gmail.com>
2023-01-19 18:12:09 +0100

    Remove construction logging from ui

--- Codecool.MarsExploration.MapExplorer/UI/SimulationStepLoggingUi.cs
+++ Codecool.MarsExploration.MapExplorer/UI/SimulationStepLoggingUi.cs
@@ -21,30 +21,19 @@ public class SimulationStepLoggingUi
             case ExplorationOutcome.InProgress:
                 foreach (var rover in simulationContext.Rovers)
                 {
-                    _logger.Step(simulationContext);
-                    _logger.Position(rover);
+                    _logger.Position(rover,simulationContext);
                 }
-
-                if (simulationContext.Constructions != null)
-                {
-                    foreach (var construction in simulationContext.Constructions)
-                    {
-                        _logger.Step(simulationContext);
-                        _logger.Construction(construction);
-                    }  
-                }
-                
                 break;
             case ExplorationOutcome.Colonizable:
-                _logger.Step(simulationContext);
+                
                 _logger.OutCome(simulationContext);
                 break;
             case ExplorationOutcome.Error:
-                _logger.Step(simulationContext);
+                
                 _logger.OutCome(simulationContext);
                 break;
             case ExplorationOutcome.Timeout:
-                _logger.Step(simulationContext);
+                
                 _logger.OutCome(simulationContext);
                 break;
         }
